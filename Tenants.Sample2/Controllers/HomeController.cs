c3c39e6268fa01ad933798827192798fb34427ae
amacdonald <amacdonald@technophobia.com>
2012-06-14 16:44:17 +0100

    Added a second tenant to the solution - dont forget to embed the spark views ;)

---
+++ Tenants.Sample2/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiTenancy.Tenants.Sample2.Controllers
+{
+  public class HomeController: MultiTenancy.Web.Controllers.HomeController
+  {
+    public override System.Web.Mvc.ActionResult Index()
+    {
+      return base.Index();
+    }
+  }
+}
---
+++ Tenants.Sample2/SampleTenant.cs
@@ -0,0 +1,28 @@
+﻿namespace MultiTenancy.Tenants.Sample2
+{
+  using MultiTenancy.Core;
+
+  /// <summary>
+    /// Tenant for "Sample2" Tenant
+    /// </summary>
+    public class SampleTenant : AbstractApplicationTenant
+    {
+        /// <summary>
+        /// Initializes a new instance of the SampleTenant class
+        /// </summary>
+        public SampleTenant()
+        {
+            // setup view engine
+            ViewEngine = DetermineViewEngine();
+
+            // setup dependency container
+            DependencyContainer = AssemblySettings.FormContainer(config => config.For<IApplicationTenant>().Singleton().Use(this));
+
+            ApplicationName = "Sample 2";
+            MinifyJavaScript = true;
+            MinifyCSS = true;
+            EnabledFeatures = null;
+            UrlPaths = new[] { "http://localhost:3455/" };
+        }
+    }
+}
